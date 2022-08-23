dc1c378033bf1c4b4f2da2aa27c401f4d7f318d9
Tiago <ottiagos@gmail.com>
2022-08-22 23:26:45 -0300

    You can return an array of elements

--- chapter2/src/index.js
+++ chapter2/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <h1>Hello Worl111d</h1>,
+    [
+      <h1>Hello Worl111d</h1>,
+      <h1>Hello Worl111d1</h1>,
+      <h1>Hello Worl111d2</h1>
+    ],
   document.getElementById('root')
 );
