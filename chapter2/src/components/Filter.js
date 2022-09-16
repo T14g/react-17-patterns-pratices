d4238aa0161a9a369abbefd5754216a2ee447af3
Tiago <ottiagos@gmail.com>
2022-09-15 22:52:53 -0300

    New filter select

--- chapter2/src/components/Filter.js
+++ chapter2/src/components/Filter.js
@@ -1,30 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Filter = ({ onChangeFirst, onChangeSeccond }) => {
+const Filter = ({ onChangeFirst, onChangeSeccond, onChangeThird }) => {
+  const [redondas, setRedondas] = useState(false);
+
   return (
     <div>
-      <select onChange={onChangeFirst}>
-        <option>Largura</option>
-        <option value="1\,30">1,30</option>
-        <option value="1\,40">1,40</option>
-        <option value="1\,50">1,50</option>
-        <option value="1\,60">1,160</option>
-        <option value="1\,70">1,170</option>
-        <option value="1\,80">1,80</option>
-        <option value="1\,90">1,90</option>
-        <option value="2\,00">2,00</option>
-      </select>
-      <select onChange={onChangeSeccond}>
-        <option>Comprimento</option>
-        <option value="1\,30">1,30</option>
-        <option value="1\,40">1,40</option>
-        <option value="1\,50">1,50</option>
-        <option value="1\,60">1,160</option>
-        <option value="1\,70">1,170</option>
-        <option value="1\,80">1,80</option>
-        <option value="1\,90">1,90</option>
-        <option value="2\,00">2,00</option>
-      </select>
+      {!redondas ? (
+        <>
+          <select onChange={onChangeFirst}>
+            <option>Largura</option>
+            <option value="1\,30">1,30</option>
+            <option value="1\,40">1,40</option>
+            <option value="1\,50">1,50</option>
+            <option value="1\,60">1,160</option>
+            <option value="1\,70">1,170</option>
+            <option value="1\,80">1,80</option>
+            <option value="1\,90">1,90</option>
+            <option value="2\,00">2,00</option>
+          </select>
+          <select onChange={onChangeSeccond}>
+            <option>Comprimento</option>
+            <option value="1\,30">1,30</option>
+            <option value="1\,40">1,40</option>
+            <option value="1\,50">1,50</option>
+            <option value="1\,60">1,160</option>
+            <option value="1\,70">1,170</option>
+            <option value="1\,80">1,80</option>
+            <option value="1\,90">1,90</option>
+            <option value="2\,00">2,00</option>
+          </select>
+        </>
+      ) : (
+        <select onChange={onChangeThird}>
+          <option>Diâmetro</option>
+          <option value="1\,30">1,30</option>
+          <option value="1\,40">1,40</option>
+          <option value="1\,50">1,50</option>
+          <option value="1\,60">1,160</option>
+          <option value="1\,70">1,170</option>
+          <option value="1\,80">1,80</option>
+          <option value="1\,90">1,90</option>
+          <option value="2\,00">2,00</option>
+        </select>
+      )}
+      <input type="checkbox" onChange={() => setRedondas(!redondas)} />
+      Filtrar toalhas redondas
     </div>
   );
 };
